56494ee800c4f3934741258281b673af37532f29
App Academy Student <student@appacademy.io>
2013-07-22 16:37:11 -0700

    Working UI

--- minesweeper_practice.rb
+++ minesweeper_practice.rb
@@ -1,4 +1,6 @@
 class MinesweeperBoard
+  attr_accessor :board
+
   def initialize(size)
     @size = size
     build_board
@@ -7,29 +9,35 @@ class MinesweeperBoard
 
   def build_board
     @board = {}
-    size[0].times do |x|
-      size[1].times do |y|
-        @board[[x + 1, y + 1]] = Tile.new(@board)
+    @size[1].times do |y|
+      @size[0].times do |x|
+        @board[[x + 1, y + 1]] = Tile.new(self, [x + 1, y + 1])
       end
     end
-    @minecount = 1 if size = [3,3]
-    @minecount = 10 if size == [9,9]
-    @minecount = 40 if size == [16,16]
-    @board.keys.sample(@minecount) { |mine_tile| @board[mine_tile].set_mine }
+  #  p @board
+    @minecount = 1 if @size == [3,3]
+    @minecount = 10 if @size == [9,9]
+    @minecount = 40 if @size == [16,16]
+    @board.keys.sample(@minecount).each { |mine_tile| @board[mine_tile].set_mine }
   end
 
   def display_board
+    print "\n "
     @board.each do |coord, tile|
-      print Tile.to_s + "\n " if coord[0] == size[0]
-      print  Tile.to_s if coord[0] != size[0]
+      print tile.display_tile + " | \n" + "_" * (@size[0] * 4) +"\n " if coord[0] == @size[0]
+      print  tile.display_tile + " | " if coord[0] != @size[0]
     end
   end
 
 end
 
 class Tile
-  def initialize(board)
-    @board = board
+  attr_accessor :coord
+
+
+  def initialize(gameboard, coordinates)
+    @coord = coordinates
+    @gameboard = gameboard
     @mined = false
     @flagged = false
     @revealed = false
@@ -43,11 +51,56 @@ class Tile
     @flagged = @flagged ? false : true
   end
 
-  def reveal
+  def reveal(visited = [])
+    visited << self
+    if adj_mine_count == 0
+      adj_tiles.each do |tile|
+        #p tile.class
+        tile.reveal(visited) unless visited.include?(tile)
+      end
+    end
+    @revealed = true
   end
 
   def adj_tiles
-  end
+    output = []
+    (-1..1).each do |x_offset|
+      #p x_offset
+      (-1..1).each do |y_offset|
+         if @gameboard.board.include?([@coord[0] + x_offset, @coord[1] + y_offset]) && !(x_offset == 0 && y_offset == 0)
+           #puts "Object:#{@gameboard.board[[@coord[0] + x_offset, @coord[1] + y_offset]]}\n coords: #{@gameboard.board[[@coord[0] + x_offset, @coord[1] + y_offset]].coord} "
+           output << @gameboard.board[[@coord[0] + x_offset, @coord[1] + y_offset]]
+         end
+       end
+     end
+     #p output
+     output
+   end
+
+   def has_mine?
+     @mined
+   end
+
+   def adj_mine_count
+     adj_tiles.map do |x|
+       x.has_mine?
+     end.count(true)
+   end
+
+   def display_tile
+     return "*" unless @revealed
+     return "M" if @mined
+     return adj_mine_count.to_s unless adj_mine_count == 0
+     return " " if adj_mine_count == 0
+     return "F" if @flagged
+
+   end
 
 end
 
+
+joe = MinesweeperBoard.new([9,9])
+#joe.board.each {|key, val| val.reveal}
+#joe.board[[1,2]].set_mine
+joe.board[[1,2]].reveal
+ joe.display_board
